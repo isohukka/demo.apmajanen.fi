cb540fd9921f706625880959923cc32f2252ace3
isohukka <30315474+isohukka@users.noreply.github.com>
2017-08-11 14:56:47 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Test environment</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+</body>
+<div id="wrapper">
+
+				<!-- All the content in the header :) -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-diamond"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>Projekteja</h1>
+								<p>Tämä WP-saitti on kovakoodattu staattisesta WP-teemaksi <a href="http://linkkitahan">GitHubiin</a> </br>Tulevaisuudessa tämän tilalle tulee toimiva template.</p>
+								<p>Muutamia projektejani:</p>
+								<ul>
+								<li><strong>Salon Spa Dreams</strong>, asiakkaan toiveiden mukaisesti toteutettu WP-saitti. Timma-integraation myötä todella suuri kävijäliikenne.</li>
+								<li><strong>Musiikkikoulu Makikalli</strong>, helposti asiakkaan päivitettävissä oleva WP-asennus.</li>
+								<li><strong>Oma valokuvaus-sivustoni</strong>, staattinen verkkosivu. Harvoin päivityksiä. Luotu ennen oman palvelimeni päivitystä.</li>
+								<li><strong>Python/Django</strong>, harjoittelusovellus joka hakee dataa käyttäen Django-frameworkkia.</li>
+								</ul>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="http://www.salonspadreams.fi/">Case: Salon Spa Dreams</a></li>
+								<li><a href="http://musiikkikoulumakikalli.fi/">Case: Musiikkikoulu Makikalli</a></li>
+								<li><a href="http://apmajanen.fi/">Case: Valokuvaus-sivustoni</a></li>
+								<li><a href="http://sadas">Case: Python/Django sovellus</a></li>
+								<!--<li><a href="#url">Case: ?</a></li>-->
+							</ul>
+						</nav>
+					</header>
+		</div>
+		<div id="bg"></div> 
+	</body>
+</html>
